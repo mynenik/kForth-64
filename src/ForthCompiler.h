fdf826bcec3c109ec4d381e33b6b516d49974a4a
Krishna Myneni <krishna.myneni@ccreweb.org>
2022-01-06 19:23:25 -0600

    Remove obsolete code.
     On branch master
    
     Changes to be committed:
    	modified:   src/ForthCompiler.h

--- src/ForthCompiler.h
+++ src/ForthCompiler.h
@@ -1,6 +1,6 @@
 // ForthCompiler.h
 //
-// Copyright (c) 1998--2019 Krishna Myneni,
+// Copyright (c) 1998--2022 Krishna Myneni,
 //   <krishna.myneni@ccreweb.org>
 //
 // This software is provided under the terms of the GNU
@@ -23,24 +23,6 @@
 #define EXECUTE_CURRENT_ONLY    2
 #define TRUE -1
 #define FALSE 0
-#define MAX_C_ERR_MESSAGES 13
-
-// Error codes; The corresponding error messages are given in
-//   the const char* array C_ErrorMessages, in ForthCompiler.cpp
-
-#define E_C_NOERROR         0x100
-#define E_C_ENDOFSTREAM     0x101
-#define E_C_ENDOFDEF        0x102
-#define E_C_ENDOFSTRING     0x103
-#define E_C_NOTINDEF        0x104
-#define E_C_OPENFILE        0x105
-#define E_C_INCOMPLETEIF    0x106
-#define E_C_INCOMPLETEBEGIN 0x107
-#define E_C_UNKNOWNWORD     0x108
-#define E_C_NODO            0x109
-#define E_C_INCOMPLETELOOP  0x10A
-#define E_C_INCOMPLETECASE  0x10B
-#define E_C_VMERROR         0x10C
 
 struct WordTemplate
 {
